729a66ba98582d7dbc1c422846d43bcc1c5549ee
Sergey Zarouski <serge.zarouski@gmail.com>
2014-07-16 22:46:46 -0400

    Fully tested code

---
+++ dist/permissions.js
@@ -0,0 +1,114 @@
+/*global exports*/
+/**
+ * @file Very simple permissions manager
+ * @author Sergey Zarouski, http://webuniverse.ca
+ */
+
+(function (root, factory) {
+	'use strict';
+	if (typeof define === 'function' && define.amd) {
+		define(['exports', '_'], factory);
+	} else if (typeof exports === 'object') {
+		factory(exports, require('_'));
+	} else {
+		factory((root.permissionsExports = {}), root._);
+	}
+}(
+	this,
+	/**
+	 * @param exports
+	 * @param {_.LoDashStatic} _
+	 */
+	function initSimplePermissions(exports, _) {
+		'use strict';
+
+		/**
+		 * @typedef {{target: String, source: String, properties: Array}} Entry
+		 */
+
+		/**
+		 * Create/append permissions in a storage
+		 * @param {Entry[]} storage
+		 * @param {String|Array} to
+		 * @param {Object} permissions
+		 */
+		function grant(storage, to, permissions) {
+			//jshint validthis: true
+			if (typeof to === 'string') {
+				to = [to];
+			}
+
+			/**
+			 * private method which handles additions
+			 * @param {String} target
+			 * @param {Array} properties
+			 * @param {String} source
+			 */
+			function addPermissions(target, properties, source) {
+				/**
+				 * @type {Entry}
+				 */
+				var targetSource = _.find(storage, {target: target, source: source});
+				if (targetSource) {
+					targetSource.properties = _.union(targetSource.properties, properties);
+				} else {
+					storage.push({
+						target: target,
+						source: source,
+						properties: properties
+					});
+				}
+			}
+
+			_.each(to, function (target) {
+				_.each(permissions, _.partial(addPermissions, target));
+			});
+		}
+
+		/**
+		 * Reduce/Remove permissions in a storage
+		 * @param {Entry[]} storage
+		 * @param {String|Array} from
+		 * @param {Object} permissions
+		 */
+		function revoke(storage, from, permissions) {
+			//jshint validthis: true
+			if (typeof from === 'string') {
+				from = [from];
+			}
+
+			/**
+			 * private method which handles removals
+			 * @param {String} target
+			 * @param {Array} properties
+			 * @param {String} source
+			 */
+			function removePermissions(target, properties, source) {
+				//remove when no properties left
+				_.remove(storage, function removeDataPermissions(entry) {
+					if (entry.target === target && entry.source === source) {
+						entry.properties = _.difference(entry.properties, properties);
+						return !entry.properties.length;
+					}
+					return false;
+				});
+			}
+
+			_.each(from, function (target) {
+				_.each(permissions, _.partial(removePermissions, target));
+			});
+		}
+
+		/**
+		 * @typedef {{grant: grant, revoke: revoke}} PermissionsExports
+		 */
+		var _exports = {
+			grant: grant,
+			revoke: revoke
+		};
+		_.extend(
+			exports,
+			_exports
+		);
+	}
+));
---
+++ test/permissionsTest.js
@@ -0,0 +1,80 @@
+/*global describe, it, expect, runs, jasmine*/
+(function () {
+	'use strict';
+	define(['permissions', '_'], function (/** PermissionsExports */permissionsExport, _) {
+		var grant = permissionsExport.grant,
+		    revoke = permissionsExport.revoke;
+
+		/**
+		 * @type {{name: string, storage: Entry[]}}
+		 */
+		var Pa = {
+				name: 'Pa',
+				storage: []
+			},
+			s = Pa.storage;
+
+		describe('test grant', function (param) {
+			it('should pass basic case', function () {
+				var str = 'whatever';
+				grant(s, Pa.name, {So: [str]});
+				var entry = _(s).find({target: Pa.name, source: 'So'});
+
+				expect(s.length).toBe(1);
+				expect(entry).toEqual(jasmine.any(Object));
+				expect(entry.properties[0]).toBe(str);
+			});
+			it('should work with multiple targets', function () {
+				var str = 'whatever';
+				grant(s, ['So', 'Do', 'Go'], {Pa: [str]});
+				var results = [
+					_(s).find({target: 'So', source: Pa.name}),
+					_(s).find({target: 'Do', source: Pa.name}),
+					_(s).find({target: 'Go', source: Pa.name})
+				];
+
+				expect(s.length).toBe(4);
+				_.each(results, function (result) {
+					expect(result).toEqual(jasmine.any(Object));
+					expect(result.properties[0]).toBe(str);
+				});
+			});
+			it('should work with multiple permission rules', function () {
+				var str1 = 'anything';
+				var str2 = 'something else';
+				grant(s, 'So', {Pa: [str1], Do: [str2]});
+				var result1 = _(s).find({target: 'So', source: Pa.name});
+				var result2 = _(s).find({target: 'So', source: 'Do'});
+
+				expect(s.length).toBe(5);
+				expect(result1.properties[1]).toBe(str1);
+				expect(result2).toEqual(jasmine.any(Object));
+				expect(result2.properties[0]).toBe(str2);
+			});
+		});
+
+		describe('test revoke', function (param) {
+			/**
+			 * @type {{name: string, storage: Entry[]}}
+			 */
+			it('should pass basic case', function () {
+				revoke(s, 'Pa', {So: ['whatever']});
+
+				expect(s.length).toBe(4);
+				expect(_(s).find({target: Pa.name, source: 'So'})).not.toBeDefined();
+			});
+            it('should work with multiple properties', function () {
+	            revoke(s, 'So', {Pa: ['anything'], Do: ['something else']});
+
+                expect(s.length).toBe(3);
+	            expect(_(s).find({target: 'So', source: Pa.name}).properties[0]).toBe('whatever');
+	            expect(_(s).find({target: 'So', source: 'Do'})).not.toBeDefined();
+            });
+            it('should work with multiple permission rules', function () {
+	            revoke(s, ['So', 'Do', 'Go'], {Pa: ['whatever']});
+
+                expect(s.length).toBe(0);
+            });
+		});
+	});
+}());
